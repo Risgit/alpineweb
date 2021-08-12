81df5a9e5322aae63f47d46b568bdb458c20bd76
Risgit <auwieder@ya.ru>
2021-08-12 10:05:14 +0300

    Update alpineweb.sh

--- alpineweb.sh
+++ alpineweb.sh
@@ -177,10 +177,12 @@ phpinstall() {
 		apk add php$phpver-litespeed php$phpver-xml php$phpver-fileinfo php$phpver-ftp php$phpver-curl php$phpver-intl php$phpver-bcmath php$phpver-gd;
 		apk add php$phpver-memcache php$phpver-memcached php$phpver-json php$phpver-iconv php$phpver-zip php$phpver-pecl-memcache php$phpver-opcache;
 		apk add php$phpver-sockets php$phpver-posix php$phpver-mysqli php$phpver-pecl-memcached php$phpver-openssl php$phpver-simplexml;
-		apk add memcached bind-tools openssh libidn;
+		apk add memcached bind-tools openssh libidn mc fail2ban;
 		rc-service memcached start;
 		rc-update add memcached;
 		pkill lsphp;
+		rc-service  fail2ban start;
+		rc-update add fail2ban;
 	fi;
 }
 
