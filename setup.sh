0ce32bb7d56b403a867f50bc1df25624647a5b46
Risgit <auwieder@ya.ru>
2023-01-03 23:10:59 +0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/ash
+
+cat >> /etc/sysctl.conf <<EOF
+net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1
+EOF
+
+sysctl -p
+
+mkdir /media/setup
+cp -a /media/sda/* /media/setup
+ 
+mkdir /lib/setup
+cp -a /.modloop/* /lib/setup
+
+/etc/init.d/modloop stop
+umount /dev/sda
+
+mv /media/setup/* /media/sda/
+mv /lib/setup/* /.modloop/
+
+echo "READY!!!"
+
