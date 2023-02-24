f0b56fd991c9ee6d58f1b868a5091503c9b2c39f
Risgit <auwieder@ya.ru>
2023-02-24 22:50:14 +0300

    Replace on virtualbox

--- setup.sh
+++ setup.sh
@@ -21,3 +21,19 @@ mv /lib/setup/* /.modloop/
 
 echo "READY!!!"
 
+#For virtualbox 
+#After setup-alpine 
+
+mkdir /media/setup
+cp -a /media/sda* /media/setup
+ 
+mkdir /lib/setup
+cp -a /.modloop/* /lib/setup
+
+/etc/init.d/modloop stop
+umount /dev/sda
+
+mv /media/setup/* /media/sda*
+mv /lib/setup/* /.modloop/
+
+echo "Now start setup-disk!!!"
